ec59766a5427be6081815f4fb6f4592e481a2d75
mariusz79 <mpawlowic@gmail.com>
2019-02-28 09:43:49 +0000

    create ask_questions function

--- quiz.py
+++ quiz.py
@@ -6,6 +6,37 @@ def show_menu():
     option = input("Enter option: ")
     return option
 
+def ask_questions():
+    questions = []
+    answers = []
+
+    with open("questions.txt", "r") as file:
+        lines = file.read().splitlines()
+    
+    for i, text in enumerate(lines):
+        if i%2 == 0:
+            questions.append(text)
+        else:
+            answers.append(text)
+
+    num_of_questions = len(questions)
+    questions_and_answers = zip(questions, answers)
+
+    score = 0
+
+    for question, answer in questions_and_answers:
+        guess = input(question + "> ")
+        if guess == answer:
+            score += 1
+            print("right!")
+            print(score)
+        else:
+            print("wrong!")
+
+    print("You got {0} correct out of {1}".format(score, num_of_questions))
+
+    
+
 def add_question():
     print(" ")
     question = input("Enter a question\n> ")
@@ -23,7 +54,7 @@ def game_loop():
     while True:
         option = show_menu()
         if option == "1":
-            print("You selected 'Ask questions'")
+            ask_questions()
         elif option == "2":
             add_question()
         elif option == "3":
